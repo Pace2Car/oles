645aa8eb43c6f5569e66034a904aa4accb81fd62
Healer <ly347265429@163.com>
2018-10-03 18:33:28 +0800

    666

---
+++ src/main/java/com/pace2car/entity/FspAnswer.java
@@ -0,0 +1,57 @@
+package com.pace2car.entity;
+
+
+public class FspAnswer {
+
+  private String id;
+  private String answer;
+  private String fspId;
+  private String examNo;
+  private String userId;
+
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+
+  public String getAnswer() {
+    return answer;
+  }
+
+  public void setAnswer(String answer) {
+    this.answer = answer;
+  }
+
+
+  public String getFspId() {
+    return fspId;
+  }
+
+  public void setFspId(String fspId) {
+    this.fspId = fspId;
+  }
+
+
+  public String getExamNo() {
+    return examNo;
+  }
+
+  public void setExamNo(String examNo) {
+    this.examNo = examNo;
+  }
+
+
+  public String getUserId() {
+    return userId;
+  }
+
+  public void setUserId(String userId) {
+    this.userId = userId;
+  }
+
+}
